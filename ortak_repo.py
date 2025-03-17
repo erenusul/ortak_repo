25cba61263c135a1680f1a4b04babd8209c2f083
NLUfuk <ufukf1998@gmail.com>
2025-03-17 23:00:12 +0300

    add ve sub fonksiyonları eklendi , menü için güncelleme yapıldı

--- ortak_repo.py
+++ ortak_repo.py
@@ -7,15 +7,22 @@ def divide(a, b):
         return "Hata: Sıfıra bölme yapılamaz!"
     return a / b
 
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+
 
 print("Hesap Makinesi")
 print("1 - Çarpma")
 print("2 - Bölme")
+print("3 - Toplama")
+print("4 - Çıkarma")
 
 try:
-    choice = int(input("Yapmak istediğiniz işlemi seçin (1/2): "))
+    choice = int(input("Yapmak istediğiniz işlemi seçin (1/4): "))
 
-    if choice not in [1, 2]:
+    if choice not in [1, 4]:
         print("Hata: Geçersiz işlem seçimi!")
     else:
         num1 = float(input("Birinci sayıyı girin: "))
@@ -23,8 +30,15 @@ try:
 
         if choice == 1:
             print(f"{num1} * {num2} = {multiply(num1, num2)}")
-        else:
+        
+        elif choice==2:
             print(f"{num1} / {num2} = {divide(num1, num2)}")
+            
+        elif choice==3:
+            print(f"{num1} / {num2} = {add(num1, num2)}")            
+
+        else:
+            print(f"{num1} / {num2} = {sub(num1, num2)}")
 
 except ValueError:
     print("Hata: Lütfen geçerli bir sayı girin!")
